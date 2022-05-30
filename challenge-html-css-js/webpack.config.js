acff0771123131002c1cf32f53e6fe6c48196ce0
gust99 <Gustavo-Gudino@jala-fundation.org>
2022-05-30 18:49:42 -0400

    Pokedex webpack final 30/05/22

--- challenge-html-css-js/webpack.config.js
+++ challenge-html-css-js/webpack.config.js
@@ -26,35 +26,6 @@ module.exports = {
             template: 'src/index.html'
         })
     ],
-    optimization: {
-        chunkIds: 'total-size',
-        emitOnErrors: true,
-        innerGraph: true,
-        minimize: true,
-        moduleIds: 'size',
-        runtimeChunk: 'single',
-        splitChunks: {
-            chunks: 'async',
-            minSize: 20000,
-            minRemainingSize: 0,
-            minChunks: 1,
-            maxAsyncRequests: 30,
-            maxInitialRequests: 30,
-            enforceSizeThreshold: 50000,
-            cacheGroups: {
-              defaultVendors: {
-                test: /[\\/]node_modules[\\/]/,
-                priority: -10,
-                reuseExistingChunk: true,
-              },
-              default: {
-                minChunks: 2,
-                priority: -20,
-                reuseExistingChunk: true,
-              },
-            },
-        },
-    },
     resolve: {
         alias: {
             Styles: path.resolve(__dirname, 'src/styles/'),
